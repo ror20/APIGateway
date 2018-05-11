3146188c22d6801c8cc823a19df52b901abfa247
574384 <574384@PC267095.cts.com>
2018-05-11 17:24:27 +0530

    Changed basic Constant

--- APIGateway/src/main/java/com/ror/apigtyway/constants/APIGatewayConstants.java
+++ APIGateway/src/main/java/com/ror/apigtyway/constants/APIGatewayConstants.java
@@ -2,5 +2,5 @@ package com.ror.apigtyway.constants;
 
 public class APIGatewayConstants {
 	public static final String AUTHORIZATION = "Authorization";
-	public static final String BASIC = "Basic";
+	public static final String BASIC = "basic";
 }
