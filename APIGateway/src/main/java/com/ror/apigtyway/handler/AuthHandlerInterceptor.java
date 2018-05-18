829652912664ce8b55e1ef4407b4e15911848947
ror20 <38526358+ror20@users.noreply.github.com>
2018-05-18 13:25:21 +0530

    Changed baseUrl and added support for exist

--- APIGateway/src/main/java/com/ror/apigtyway/handler/AuthHandlerInterceptor.java
+++ APIGateway/src/main/java/com/ror/apigtyway/handler/AuthHandlerInterceptor.java
@@ -1,11 +1,12 @@
 package com.ror.apigtyway.handler;
 
+import static com.ror.apigtyway.constants.APIGatewayConstants.AUTHORIZATION;
+import static com.ror.apigtyway.constants.APIGatewayConstants.BASIC;
+
 import java.util.Base64;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static com.ror.apigtyway.constants.APIGatewayConstants.AUTHORIZATION;
-import static com.ror.apigtyway.constants.APIGatewayConstants.BASIC;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -14,19 +15,16 @@ public class AuthHandlerInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		String authHeader = request.getHeader(AUTHORIZATION);
 		System.out.println(request.getRequestURL());
-		if (authHeader == null) {
-			return false;
+		if (authHeader != null && !authHeader.isEmpty()) {
+			if (authHeader.toLowerCase().contains(BASIC)) {
+				System.out.println("basic is present");
+				String tokens[] = authHeader.split(" ");
+				byte[] decodedToken = Base64.getDecoder().decode(tokens[1]);
+				String userNamePass = new String(decodedToken);
+				return true;
+			}
 		}
-		if (authHeader.isEmpty()) {
-			return false;
-		}
-		if (authHeader.toLowerCase().contains(BASIC)) {
-			System.out.println("basic is present");
-			String tokens[] = authHeader.split(" ");
-			byte[] decodedToken = Base64.getDecoder().decode(tokens[1]);
-			String userNamePass = new String(decodedToken);
-			return true;
-		}
+		response.setStatus(401);
 		return false;
 	}
 }
