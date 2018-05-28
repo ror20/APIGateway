248eda0c1342d14d93908e0da42d289a795aa2bd
ror20 <38526358+ror20@users.noreply.github.com>
2018-05-28 18:21:26 +0530

    Update RORFilter.java

--- APIGateway/src/main/java/com/ror/filter/RORFilter.java
+++ APIGateway/src/main/java/com/ror/filter/RORFilter.java
@@ -42,11 +42,13 @@ public class RORFilter implements Filter {
 		System.out.println("Enabling CORS!!");
 		HttpServletResponse response1 = (HttpServletResponse) response;
 
-		//response1.setHeader("Access-Control-Allow-Origin", "*");
+		HttpServletRequest request1 = (HttpServletRequest) request;
+
+		response1.setHeader("Access-Control-Allow-Origin", "*");
 
 		response1.setHeader("Access-Control-Allow-Methods", "POST,PUT, GET, OPTIONS, DELETE");
 
-		response1.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response1.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
 
 		response1.setHeader("Access-Control-Expose-Headers", "x-requested-with");
 		chain.doFilter(request, response1);
