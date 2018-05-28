b83198225e6522f824f909effd0c3d774e85af24
ror20 <38526358+ror20@users.noreply.github.com>
2018-05-28 18:38:21 +0530

    added options for cors

--- APIGateway/src/main/java/com/ror/apigtyway/controller/APIAccessGtway.java
+++ APIGateway/src/main/java/com/ror/apigtyway/controller/APIAccessGtway.java
@@ -1,6 +1,7 @@
 package com.ror.apigtyway.controller;
 
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,6 +49,14 @@ public class APIAccessGtway {
 	@RequestMapping(value = "/checkUserExist/{id}", method = RequestMethod.GET, produces = "application/json")
 	public boolean checkUserExists(@PathVariable("id") String userId) {
 		return apiGatewayServiceImpl.checkUserExists(userId);
+	}
+	
+	@RequestMapping(value= "/api/**", method=RequestMethod.OPTIONS)
+	public void corsHeaders(HttpServletResponse response) {
+	    response.addHeader("Access-Control-Allow-Origin", "*");
+	    response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	    response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+	    response.addHeader("Access-Control-Max-Age", "3600");
 	}
 
 }
